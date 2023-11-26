9e9f71e2a7791e85af82dc31c92efea4832f9c46
GermanS04 <german.salasmar@gmail.com>
2023-11-25 19:34:12 -0600

    fix(SearchBar): Search for name in the patients list

--- hospit-ai/app/components/TablaPacientes/TablaPacientes.tsx
+++ hospit-ai/app/components/TablaPacientes/TablaPacientes.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import SearchBar from '../SearchBar/SearchBar';
 
 const TablaPacientes = () => {
   const [data, setData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
     const fetchData = async () => {
         try {
-            const res = await fetch('http://localhost:3001/api/pacienteslista');
+            const res = await fetch('http://localhost:3001/api/pacienteslista?search=${searchQuery}');
             const allData = await res.json();
 
             setData(allData);
@@ -23,20 +25,18 @@ const TablaPacientes = () => {
         const intervalID = setInterval(fetchData, 10000);
 
         return () => clearInterval(intervalID);
-    }, []);
+    }, [searchQuery]);
+
+    const handleSearch = (query : any) => {
+        setSearchQuery(query)
+    }
 
   return (
     <div className='w-full'>
       <div className='w-[80%] mx-auto text-black'>
         <div className='w-full'>
           <div className='flex justify-center items-centers'>
-            <input
-              type='text'
-              placeholder="Buscar por nombre"
-              className='w-full font-primary text-md rounded-lg
-              px-4 py-3 text-black my-4 border border-gra bg-gray-50
-              shadow-lg outline-none'
-            />
+            <SearchBar onSearch={handleSearch}/>
           </div>
           <div>
             <table className="w-full overflow-auto">
@@ -51,13 +51,15 @@ const TablaPacientes = () => {
               </thead>
               <tbody>
                 {data.map((item:any, index: number) => (
+                  (item.Nombre.toLowerCase().includes(searchQuery.toLowerCase()) && (
                   <tr className={index % 2 === 0 ? "bg-white" : "bg-gray-200" } key={item.Cuarto} >
                     <td className="text-center border">{item.Cuarto}</td>
                     <td className="text-center border">{item.Nombre}</td>
                     <td className="text-center border">{item.EstatusPedido === 0 ? "Sin Pedido" : "Pendiente"}</td>
-                    <td className="text-center border">{item.Temperatura}</td>
+                    <td className="text-center border">{item.Temperatura}° C</td>
                     <td className="text-center border">{item.Pulso}</td>
                   </tr>
+                  ))
                 ))}
               </tbody>
             </table>
