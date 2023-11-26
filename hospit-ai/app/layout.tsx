889012c6a8f7db4149588be50e433fb14e1d934c
GermanS04 <german.salasmar@gmail.com>
2023-11-25 20:05:04 -0600

    style(font): Change from Inter to Roboto

--- hospit-ai/app/layout.tsx
+++ hospit-ai/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'HospitAI',
@@ -27,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
